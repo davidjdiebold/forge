6e16a1641d70d59c3855fcf13949fe06d889b9a4
David Diebold <d.diebold@criteo.com>
2026-04-06 19:31:52 +0200

    install git skip yes

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt install -y maven
 
 mkdir forge
 cd forge
-git clone git@github.com:davidjdiebold/forge.git
+GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" git clone git@github.com:davidjdiebold/forge.git
 cd forge
 git fetch
 git checkout gamerunnerapi
